4436edd059cbac983874eefb5009615f3029c23b
=jainam patel <pateljainams122@gmail.com>
2025-01-03 23:54:40 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/Header"
-import GridPattern from "@/components/ui/grid-pattern"
+
 import Hero from "@/components/Hero"
 export default function Home(){
     return(
